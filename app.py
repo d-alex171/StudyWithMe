3ddef568be02f61c7344c588d0ad8f12dcb1511b
Alex <alexey.ddudarev@gmail.com>
2024-01-20 14:48:41 -0800

    made the log in page

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 
+from flask import request, redirect
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    print("The username is '" + username + "'")
+    return redirect('/')
 
 
 if __name__ == '__main__':
