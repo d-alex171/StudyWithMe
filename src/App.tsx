173ad7b8228a4d031d146361fb6f2c299b3dc86c
lancesoliman <lsolim01@student.ubc.ca>
2024-01-20 20:04:58 -0800

    added ikb map using Mappedin

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Basic from "./Basic";
+import React from "react";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ display: "flex", gap: "12px" }}>
+        Examples:
+        <Link to="/">Basic</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Basic />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
