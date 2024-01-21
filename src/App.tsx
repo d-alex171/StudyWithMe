50527e218c06ebbd7f6fb11af8cccc6d620366d9
lancesoliman <lsolim01@student.ubc.ca>
2024-01-21 02:27:40 -0800

    removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ export default function App() {
   return (
     <BrowserRouter>
       <div style={{ display: "flex", gap: "12px" }}>
-        Examples:
-        <Link to="/">Basic</Link>
       </div>
       <Routes>
         <Route path="/" element={<Basic />} />
--- src/Basic.tsx
+++ src/Basic.tsx
@@ -11,12 +11,8 @@ import useMapView from "./hooks/useMapView";
 import useVenueMaker from "./hooks/useVenueMaker";
 import "./styles.css";
 
-/* This demo shows you how to configure and render a map. */
 export default function BasicExample() {
-  /*
-   * API keys and options for fetching the venue must be memoized
-   * to prevent React from re-rendering excessively.
-   */
+
   const credentials = useMemo<TGetVenueMakerOptions>(
     () => ({
       mapId: "65ac2f9eca641a9a1399dc12",
@@ -26,33 +22,30 @@ export default function BasicExample() {
     }),
     []
   );
-  // The venue object contains all the iterable data for the map
+
   const venue = useVenueMaker(credentials);
 
   const mapOptions = useMemo<TMapViewOptions>(
     () => ({
-      backgroundColor: "#CFCFCF", // Background colour behind the map
+      backgroundColor: "#CFCFCF", // bg colour behind map
     }),
     []
   );
-  // The mapView is the entrypoint to controling the map
+
   const { elementRef, mapView } = useMapView(venue, mapOptions);
 
-  /* Map setup should be done in a useEffect */
   useEffect(() => {
-    // Check that the map and venue were created successfully
     if (!mapView || !venue) {
       return;
     }
 
-    // Label all spaces and desks which have a name
     mapView.FloatingLabels.labelAllLocations();
   }, [mapView, venue]);
 
   return (
     <div id="app">
       <div id="ui">
-        {/* Render some map details to the UI */}
+        {/* render map details to ui */}
         {venue?.venue.name ?? "Loading..."}
         {venue && (
           <select
@@ -61,7 +54,7 @@ export default function BasicExample() {
                 return;
               }
 
-              // When the floor select changes we can find and set the map to that ID
+              // set floor to changed id
               const floor = venue.maps.find((map) => map.id === e.target.value);
               if (floor) {
                 mapView.setMap(floor);
